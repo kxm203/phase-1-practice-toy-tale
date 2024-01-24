d93ca56c220404b2fb27ed6d6aba50a292758852
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-24 06:45:34 -0500

    I followed along with Emiley and that helped because there is a lot to take in!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const toyContainer = document.getElementById('toy-collection')
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +15,87 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function updateLikes(toy, likesElement) {
+  toy.likes++
+
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      likes: toy.likes
+    })
+  })
+    .then((resp) => resp.json())
+    .then((updatedToy) => likesElement.textContent = `${updatedToy.likes} likes`)
+}
+
+
+function renderToy(toy) {
+  console.log(toy)
+  const toyCard = document.createElement('div')
+  toyCard.className="card"
+
+toyCard.innerHTML = `
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  `
+  const button = document.createElement('button')
+  button.className = "like-btn"
+  button.id = toy.id
+  button.textContent = "Like ❤️"
+
+  const likesElement = document.createElement('p')
+  likesElement.textContent = `${toy.likes} Likes`
+  toyCard.append(likesElement, button)
+
+
+  button.addEventListener('click', () => updateLikes(toy, likesElement ))
+
+  toyContainer.append(toyCard)
+}
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then((resp) => resp.json())
+  .then((toys) => toys.forEach(renderToy))
+}
+
+function handleNewToy(event) {
+  event.preventDefault()
+  const toyName = event.target.name.value
+  const image = event.target.image.value
+
+  const newToyObj = {
+    name: toyName,
+    image,
+    likes: 0
+  }
+
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(newToyObj)
+  })
+  .then((resp) => resp.json())
+  .then((newToy) => {
+    renderToy(newToy)
+    event.target.reset()
+  })
+}
+function listenForNewToy() {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', handleNewToy)
+}
+
+function init() {
+  getToys()
+  listenForNewToy()
+}
+
+init()
